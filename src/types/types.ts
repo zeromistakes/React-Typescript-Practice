cc13c06e8bf3f2aac0bd4ea2cbe2a807f703c79b
Roman Voronin <zeromistakes4@gmail.com>
2022-10-08 15:52:26 +0300

    final commit

---
+++ src/types/types.ts
@@ -0,0 +1,19 @@
+
+export interface IUser {
+    id: number;
+    name: string;
+    email: string;
+    address: IAddress;
+}
+
+export interface IAddress {
+    street: string;
+    city: string;
+    zipcode: string;
+}
+
+export interface ITodo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
